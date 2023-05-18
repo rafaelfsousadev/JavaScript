18de874575db9c7239e798e8638a6d792b348631
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-05-18 18:57:02 -0300

    novo

---
+++ curso-CEV/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+//condição simples
