fb774c90e5fa4d7c76cd510eac555060ccad8538
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-05-20 15:56:44 -0300

    fez

---
+++ curso-CEV/desafio01/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 11
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        msg.innerHTML +=` <strong>BOM DIA!</strong>`
+        img.src = 'imagens/Manhã.jpg'
+        document.body.style.background = '#dbaf80'
+    
+    }else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML +=` <strong>BOA TARDE!</strong`
+        img.src="imagens/Tarde.jpg" 
+
+        document.body.style.background = '#f77101'
+    } else{
+        msg.innerHTML +=` <strong>BOA NOITE!</strong>`
+        img.src = 'imagens/Noite.jpg'
+        document.body.style.background = '#0F1B46'
+    }
+}
