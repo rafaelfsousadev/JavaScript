575c8927b32ba3f7d8bdce9dee288a762cb569d3
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-05-23 16:09:09 -0300

    23/05

---
+++ curso-CEV/Aula13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+while (c<= 10){
+    console.log('Tudo bem?')
+    c++
+}
+
+
+
+
+
+
+
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
