fa6981c18f1105c2e0bd9ccc475b095006428104
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-05-23 09:12:07 -0300

    att

--- curso-CEV/desafio02/script.js
+++ curso-CEV/desafio02/script.js
@@ -9,12 +9,37 @@ function Verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var género = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        var img = document.getElementById('imagem')
         if (fsex[0].checked){
             género = 'Homem'
-        }else{
+            if (idade >=0 && idade < 10){
+                //criança
+                img.src="imagens/bebe-homem.jpg"
+            }else if (idade < 21){
+                 //jovem
+                 img.src="imagens/jovem-homem.jpg"
+            }else if (idade< 50){
+                //adulto
+                img.src="imagens/Adulto-homem.jpg"
+            }else{
+                //idoso
+                img.src="imagens/idoso-homem.jpg"
+            }
+        }else if(fsex[1].checked){
             género ='Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.src="imagens/bebe-mulher.jpg"
+            }else if (idade < 21){
+                 //jovem
+                img.src="imagens/jovem-mulher.jpg"
+            }else if (idade< 50){
+                //adulto
+                img.src="imagens/Adulto-mulher.jpg"
+            }else{
+                //idoso
+                img.src="imagens/idoso-mulher.jpg"
+            }
         }
         res.innerHTML = `Detectamos ${género} de ${idade} anos.`
     }
