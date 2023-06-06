58956cae1363c22abef2a8377d6b5bd65ba69463
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-06-06 12:56:41 -0300

    em?

---
+++ curso-matheus/objetos/js/script.js
@@ -0,0 +1,11 @@
+var obj = {
+    nome:'Rafael',
+    idade:20,
+    profissão:'Programador',
+}
+
+console.log(obj)
+
+console.log(obj.idade)
+
+console.log(`Meu nome é ${obj.nome}, minha idade é ${obj.idade}, é eu sou ${obj.profissão}`)
