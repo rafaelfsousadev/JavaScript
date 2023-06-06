a905ba9c58a6340399980866a2d416f89ca189c3
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-06-06 16:26:01 -0300

    att20

---
+++ curso-matheus/If/js/script.js
@@ -0,0 +1,8 @@
+var idade = 17
+
+
+if(idade >= 18){
+ console.log('Pode fazer a carteita de habilitação')
+}else{
+    console.log('você e menor de idade!')
+}
---
+++ curso-matheus/array/js/script.js
@@ -0,0 +1,3 @@
+var arr =['Rafael',5,'Gamer']  // objeto => {}
+
+console.log(`Meu nome é ${arr[0]}, tenho ${arr[1]} anos de idade. meu hobbie e ser ${arr[2]}`)
