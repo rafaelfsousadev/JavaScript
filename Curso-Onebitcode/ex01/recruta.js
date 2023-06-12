537a2f7a1a97d5255bd7370dcd1b4f48a4af09ca
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-06-12 11:33:08 -0300

    noo

---
+++ Curso-Onebitcode/ex01/recruta.js
@@ -0,0 +1,5 @@
+const primeiroNome =  window.prompt('Qual seu nome?')
+const sobrenome = window.prompt('Qual seu sobrenome?')
+const anoNascimento = window.prompt('Qual ano você nasceu?')
+
+window.alert(`seu nome é: ${primeiroNome} \nseu sobrenome é: ${sobrenome} \nsua idade é: ${2023 - anoNascimento}`)
