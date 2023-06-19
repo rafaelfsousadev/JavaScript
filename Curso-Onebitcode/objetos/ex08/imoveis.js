b23634805729ed2820e251ffdaa76aada3aef44f
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-06-19 11:09:32 -0300

    feito

--- Curso-Onebitcode/objetos/ex08/imoveis.js
+++ Curso-Onebitcode/objetos/ex08/imoveis.js
@@ -1,27 +1,51 @@
-let quantidadeImoveis = ''
-let totalImoveis = 0
 let opcao = ''
-let imgoveis = {}
+const imoveis = []
 
 do{
-    const opcao = prompt(
-        `Escolha o que fazer:\n
+    opcao = prompt(
+        `Bem vindo(a) ao cadastro de imóveis!\n
+        Total de Imóveis: ${imoveis.length}\n
+        \nEscolha uma Opção:
         \n1. cadastrar um novo imóvel
-        \n2. quantidade de imovéis cadastrados
+        \n2. Listar imóveis
         \n3. Sair`
     )
 
     switch(opcao){
         case '1':
-            let nomeDoImovel = prompt('Qual o nome do imóvel?')
-            quantidadeImoveis += `- ${nomeDoImovel}`
+            const imovel = {}
+
+            imovel.proprietario = prompt('Informe o nome do proprietário do imóvel:')
+            imovel.quartos = prompt('Quantos quertos possui o imóvel?')
+            imovel.banheiros = prompt('Quantos banheiros possui o imóvel?')
+            imovel.garagem = prompt('O imóvel possui garagem? (Sim/Não)')
+
+            const confirmacao = confirm(
+                `Salvar este imóvel?\n
+                \nProprietário: ${imovel.proprietario}
+                \nQuartos: ${imovel.quartos}
+                \nBanheiros: ${imovel.banheiros}
+                \nPossui garagem?: ${imovel.garagem}`
+            )
+
+            if(confirmacao){
+                imoveis.push(imovel)
+            }else{
+                alert('Voltando ao menu!')
+            }
+            
+
             break
         case '2':
-            if(quantidadeImoveis =''){
-                alert('Não tem nenhum Imóvel cadastrado!')
-            }else{
-                alert(quantidadeImoveis)
-            }
+           for(let i = 0; i < imoveis.length; i++){
+            alert(
+                `Imóvel ${i + 1}
+                \nProprietário: ${imoveis[i].proprietario}
+                \nQuartos: ${imoveis[i].quartos}
+                \nBanheiros: ${imoveis[i].banheiros}
+                \nPossui garagem? ${imoveis[i].garagem}`
+            )
+           }
             break
         case '3':
             alert('Saindo...')
