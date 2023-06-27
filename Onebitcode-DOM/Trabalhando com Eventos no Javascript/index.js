4b31817632df0ea811f21094b635b1fad4372ed3
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-06-27 15:12:25 -0300

    feito

---
+++ Onebitcode-DOM/Trabalhando com Eventos no Javascript/index.js
@@ -0,0 +1,9 @@
+function register(ev){
+    console.log(ev)
+}
+
+const button = document.getElementById('register-button')
+
+button.addEventListener('click', function(){
+    alert('Botão clikado!')
+})
