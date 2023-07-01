872670522aa95cf83906f83b8a1424ee36221c82
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-07-01 19:13:58 -0300

    feu

--- Onebitcode-DOM/Manipulando Atributos com Javascript/index.js
+++ Onebitcode-DOM/Manipulando Atributos com Javascript/index.js
@@ -1,7 +1,7 @@
 const input = document.getElementById('input')
 
 document.getElementById('value').addEventListener('click', function(){
-    if(input.value === ''){
-        input.value === 'Olá Mundo!'
+    if(input.value == ''){
+        input.value == 'Olá Mundo!'
     }
 })
