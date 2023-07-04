8f41709078ad4f2c6e20b63272f817259c4f871b
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2023-07-04 12:56:25 -0300

    feitosa

---
+++ Onebitcode-DOM/calc-js/index.js
@@ -0,0 +1,39 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.querySelector('input')
+const resultInput = document.getElementById('result')
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll('.charKey').forEach(function (charkayBnt){
+    charkayBnt.addEventListener('click', function(){
+        const value = charkayBnt.dataset.value
+        input.value += value    
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function(){
+    input.value = ''
+    input.focus()
+})
+
+input.addEventListener('keydown', function(ev){
+    ev.preventDefault()
+    if(allowedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }
+    if(ev.key === 'Backspace'){
+        input.value = input.value.slice(0, -1)
+    }
+    if(ev.key === 'Enter'){
+        calculete()
+    }
+})
+
+document.getElementById('equal').addEventListener('click', calculete)
+
+function calculete(){
+    const result = eval(input.value)
+    resultInput.value = result
+}
