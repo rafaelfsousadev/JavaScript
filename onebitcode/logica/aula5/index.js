ea6c57e11888019f195a6c2bc43d965c60dc608e
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2024-02-02 23:21:20 -0300

    feito

---
+++ onebitcode/logica/aula5/index.js
@@ -0,0 +1,16 @@
+
+const resultado = prompt('Escolha uma alternativa: \na) \nb) \nc)')
+
+switch(resultado){
+    case 'a':
+        alert('resultado é "a"')
+        break
+   case 'b':
+        alert('resultado é "b"')
+        break
+    case 'c':
+        alert('resultado é "c"')
+        break
+    default:
+        alert('finalizando')
+}  
