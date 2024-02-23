6f2e5101087a21f6e2ec0ec686bb4fcc7f2dc9cd
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2024-02-22 23:23:06 -0300

    erro

--- onebitcode/estrutura-funções/ex5/index.js
+++ onebitcode/estrutura-funções/ex5/index.js
@@ -1,16 +1,15 @@
 const vagas = []
 
-function listarVagas(){
-    const vagasEmTexto = vagas.reduce(function(textoFinal, vaga, indice){
-        `${textoFinal +=indice}.`
-        textoFinal +=vaga.nome
-        textoFinal +=  `(${vaga.candidatos.length} candidatos)\n `
-        return textoFinal
-    }, '')
-
+function listarVagas() {
+    const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => {
+      textoFinal += indice + ". "
+      textoFinal += vaga.nome
+      textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
+      return textoFinal
+    }, "")
+  
     alert(vagasEmTexto)
-}
-
+  }
 
 function novaVaga(){
     const nome = prompt('Informe o nome da vaga:')
@@ -28,24 +27,21 @@ function novaVaga(){
      alert('Vaga criada!')
 }
 
-function exibirVaga(){
-    const indice = prompt('Informe o indice da vaga que deseja exibir:')
+function exibirVaga() {
+    const indice = prompt("Informe o índice da vaga que deseja exibir:")
     const vaga = vagas[indice]
-
-    const condidatosEmTexto = vaga.candidatos.reduce(function(textoFinal, candidato,){
-        `return textoFinal\n
-        ${candidato}`
-    }, '')
-
+  
+    const candidatosEmTexto = vaga.candidatos.reduce((textoFinal, candidato) => textoFinal + "\n - " + candidato, "")
+  
     alert(
-        "Vaga nº " + indice +
-        "\nNome: " + vaga.nome +
-        "\nDescrição: " + vaga.descricao +
-        "\nData limite: " + vaga.dataLimite +
-        "\nQuantidade de candidatos: " + vaga.candidatos.length +
-        "\nCandidatos inscritos:" + candidatosEmTexto
-      )
-}
+      "Vaga nº " + indice +
+      "\nNome: " + vaga.nome +
+      "\nDescrição: " + vaga.descricao +
+      "\nData limite: " + vaga.dataLimite +
+      "\nQuantidade de candidatos: " + vaga.candidatos.length +
+      "\nCandidatos inscritos:" + candidatosEmTexto
+    )
+  }
 
 function inscreverCandidato(){
     const candidato = prompt('Informe nome do candidato(a)')
@@ -92,5 +88,38 @@ function exibirMenu() {
   
     return opcao
   }
+
+  function execultar(){
+    let opcao = ''
+    do {
+
+        opcao = exibirMenu()
+
+        switch(opcao){
+            case'1': 
+                listarVagas()
+                break
+            case'2':
+                novaVaga()
+                break
+            case'3':
+                exibirVaga()
+                break
+            case'4':
+                inscreverCandidato()
+                break
+            case'5':
+                excluirVaga()
+                break
+            case'6':
+                alert('Encerrando...')
+                break
+            default:
+                alert('Opção invalida!')
+        }
+    }while(opcao !== '6')
+  }
+
+  execultar()
 
 
