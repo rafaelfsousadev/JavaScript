1123fb1a88f3dab0c746fafc87220a8390e0b52a
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2024-08-08 21:02:07 -0300

    aa

--- onebitcode/JSModerno/ex3/birthday.js
+++ onebitcode/JSModerno/ex3/birthday.js
@@ -3,10 +3,13 @@ const dayjs = require('dayjs')
 function birthday(date){
     const birthday = dayjs(date)
     const tadoy = dayjs()
+    const daysToNextBirthday = nextBirthday.diff(tadoy, 'day')
 
     const ageInYears = tadoy.diff(birthday, 'year')
     const nextBirthday = birthday.add(ageInYears + 1, 'year')
     console.log(`Idade: ${ageInYears}`)
+    console.log(`Proximo aniversário: ${nextBirthday.format('DD/MM/YYYY')}`)
+    console.log(`Dias até completar ${ageInYears + 1} anos: ${daysToNextBirthday}`)
 }
 
 birthday('2002-06-08')
