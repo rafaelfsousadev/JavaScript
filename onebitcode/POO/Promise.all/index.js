8158ddf63f013f8e422f53a49ddce97e48c20b6d
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2024-09-13 16:12:41 -0300

    coco

---
+++ onebitcode/POO/Promise.all/index.js
@@ -0,0 +1,20 @@
+function waitFor(seconds){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve()
+        }, seconds *1000)
+    })
+}
+
+const numbers = [4,5,9,13,77]
+
+const squares = numbers.map(async (number)=>{
+   await waitFor(2)
+   return number * number
+})
+
+console.log(squares)
+
+Promise.all(squares).then(results =>{
+    console.log(results)
+})
